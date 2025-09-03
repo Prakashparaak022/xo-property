9ba7ea7501b31fe4e7754345e7c234dbf9cae8bb
Prakashparaak022 <prakashparaak@gmail.com>
2025-09-03 15:13:51 +0530

    css - bundle removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@/assets/css/fontawesome.min.css";
 import "@/assets/css/loader.css";
 import "@/assets/css/animations.css";
 
