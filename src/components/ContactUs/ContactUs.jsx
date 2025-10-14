c74d77dc8167e6918aa29e2667883e507031514e
Prakashparaak022 <prakashparaak@gmail.com>
2025-10-14 11:55:01 +0530

    contact us

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,36 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 import StyledButton from "../Common/StyledButton";
 import Animate from "../Common/Animate";
+import { FaPhone } from "react-icons/fa";
 
 const ContactUs = () => {
+  const title = "Connect With KNMG Property";
+  const subTitle = "Your journey starts with a conversation.";
+  const content =
+    "Have a question or need guidance in Dubai’s real estate market? The KNMG Property team is here to help. Fill out the form, and our advisors will reach out to you soon to make your journey seamless.";
+  const description = "Let’s connect today!";
   return (
     <div className="flex flex-col md:flex-row justify-between items-center p-4 md:p-10 gap-4 md:gap-8">
       {/* Left Section - Image */}
       <div className="md:w-1/2">
         <Animate>
-          <h1 className="text-2xl md:text-4xl font-semibold mb-5">
-            Contact us
-          </h1>
+          <h1 className="text-2xl md:text-4xl font-semibold mb-5">{title}</h1>
         </Animate>
         <Animate>
-          <p className="mt-4 text-lg text-gray-600">
-            We’d love to hear from you! Whether you have a question about our
-            services, want to learn more about the dynamic Dubai real estate
-            market, or simply want to get to know us better, the team at KNMG
-            Property is here to assist.
-          </p>
+          <p className="mt-4 text-lg text-gray-600 font-semibold">{subTitle}</p>
         </Animate>
         <Animate>
-          <p className="mt-4 text-lg text-gray-600">
-            Fill out the form below, and one of our friendly experts will reach
-            out to you shortly. We’re excited to connect with you and help you
-            with all your real estate needs.
-          </p>
+          <p className="mt-4 text-lg text-gray-600">{content}</p>
         </Animate>
         <Animate>
-          <p className="mt-4 text-lg text-gray-600">
-            Let’s start a conversation!
-          </p>
+          <p className="mt-4 text-lg text-gray-600">{description}</p>
         </Animate>
         <Animate>
           <div className="mt-8">
@@ -55,14 +48,26 @@ const ContactUs = () => {
 
         <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-6 text-gray-800">
           <div>
-            <h2 className="text-xl font-semibold">KNMG Property</h2>
+            <h2 className="text-xl font-semibold">
+              KNMG Property Headquarters
+            </h2>
             <p className="text-gray-600 mt-1">
               Office No. 202, Daytona House, Motor City, Dubai, UAE
             </p>
           </div>
           <div>
-            <p className="font-semibold text-black">(04) 565 3718</p>
-            <p className="text-gray-600">info@knmgproperty.ae</p>
+            <div className="flex items-center justify-start space-x-1">
+              <button className="bg-white text-black rounded-full">
+                📞
+              </button>
+              <p className="font-semibold text-black"> (04) 565 3718</p>
+            </div>
+            <div className="flex items-center justify-start space-x-1">
+              <button className="bg-white text-black rounded-full">
+                📧
+              </button>
+              <p className="text-gray-600">info@knmgproperty.ae</p>
+            </div>
           </div>
         </div>
       </div>
